b7d50e1647a887546635e1ca550165f225c6c3a4
Codensity <codensity30@gmail.com>
2023-12-29 17:05:47 +0530

    mongo-jwt partial assignment done

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,27 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect("mongodb://127.0.0.1/db-jwt");
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  username: String,
+  password: String,
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  username: String,
+  password: String,
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,58 @@
 const { Router } = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db/index");
+
 const router = Router();
+const secretKey = "password";
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+
+//Sign Up route which creates account and store in db
+router.post("/signup", async (req, res) => {
+  try {
+    // encrypting the password using bcrypt
+    const password = await bcrypt.hash(req.body.password, 10);
+
+    // storing the user in the db
+    await Admin.create({
+      username: req.body.username,
+      password: password,
+    });
+
+    res.json({ msg: "Admin created successfully" });
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+// Sign In route which on successful sign in returns the jwt
+router.post("/signin", async (req, res) => {
+  try {
+    const admin = await Admin.findOne({ username: req.body.username });
+    if (!admin) {
+      res.status(401).json({ error: "Invalid email" });
+      return;
+    }
+    const result = await bcrypt.compare(req.body.password, admin.password);
+    if (!result) {
+      res.status(401).json({ error: "Invalid password or email" });
+      return;
+    }
+    const token = jwt.sign({ username: admin.username }, secretKey);
+    res.json({ token: token });
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
 });
 
 module.exports = router;
